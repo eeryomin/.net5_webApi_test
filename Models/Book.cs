f430dbcc91fc9c9143e9503ec4de09772fafb328
eeryomin <eevl2007@mail.ru>
2021-12-01 21:18:20 +0400

    Add files of nosql service.
    Names of fields in Book.cs model must match with keys document's of books collection.

---
+++ Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CoreApi1.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("bookName")]
+        public string BookName { get; set; }
+
+        public string author { get; set; }
+
+        public string[] genres { get; set; }
+    }
+}
